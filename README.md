4c2e1172a7b86878ef8984d3a9450903e631be24
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-11 22:54:34 -0400

    autocomplete test

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ It is setting a basic API consuming app with a rating system using Material UI a
 <br>
 <br>
 This repo has everything up to 46:00 mark where the instructor starts to implement the API's the app will consume. As I do not plan to use these specific API's and this is meant to be a generic tool I will leave the template at this part of the build.
+<br>
+<br>
+NPM Packages:
+<br>
+https://www.npmjs.com/package/use-places-autocomplete
+<br>
+https://www.npmjs.com/package/@react-google-maps/api?activeTab=readme
+<br>
+https://www.npmjs.com/package/@reach/combobox
+<br>
+Youtube Tutorials:
+<br>
+https://www.youtube.com/watch?v=9e-5QHpadi0&list=PL2rFahu9sLJ2QuJaKKYDaJp0YqjFCDCtN&ab_channel=GoogleMapsPlatform
+<br>
+https://www.youtube.com/watch?v=I9F-CMiKOz4&ab_channel=Weibenfalk
+<br>
+https://www.youtube.com/watch?v=s4n_x5B58Dw&ab_channel=GoogleMapsPlatform
+<br>
+https://developers.google.com/maps/documentation/javascript/places-autocomplete#restrict-predictions-to-a-specific-country
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,13 +1,69 @@
 import React from "react";
-import useStyles from "./styles";
+import { useState, useMemo } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-function PlaceDetails({ place }) {
-  const classes = useStyles();
+function PlaceDetails() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 43.6532, lng: -79.3832 }), []);
+  const [selected, setSelected] = useState(null);
+
   return (
-    <div>
-      <h1 className={classes.mapContainer}>{place.name}</h1>
-    </div>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+
+      <GoogleMap zoom={10} center={center} mapContainerStyle="map-container">
+        {selected && <Marker position={selected} />}
+      </GoogleMap>
+    </>
   );
 }
 
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  return (
+    <Combobox>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        placeholder="Enter an address"
+      />
+    </Combobox>
+  );
+};
 export default PlaceDetails;
